37f81e52416285e45fcc2c02340b45da042dff9e
peter <peter@peter-ThinkPad-X201-Tablet.(none)>
2013-08-05 16:07:08 +0100

    Working for local server

--- sensorv1.ino
+++ sensorv1.ino
@@ -1,8 +1,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-byte mac[] = { 0x8E, 0x8D, 0xBE, 0x8F, 0xFE, 0xED };
-char server[] = "ec2-54-213-70-98.us-west-2.compute.amazonaws.com";
+byte mac[] = { 0x86, 0x8D, 0xBE, 0x8F, 0xFE, 0xED };
+//char server[] = "ec2-54-213-70-96.us-west-2.compute.amazonaws.com";
+char server[] = "10.32.24.114";
 int myId = -1;
 char myName[] = "PetersDesk";
 int sensorPin = A0;
@@ -14,21 +15,19 @@ char contype[] = "Content-Type: application/x-www-form-urlencoded";
 char conlen[] = "Content-Length: 8";
 char conclose[] = "Connection: close";
 
+int debugInfoTCP = 0;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Setup Beginning");
+  Serial.println(".");
   if(Ethernet.begin(mac)==0)
     Serial.println("Failed to configure Ethernet with DHCP");
   else 
     Serial.println("Phew");
   delay(1000);
-  Serial.println("connecting...");
   
-  while(!client.connect(server,4000)){
-    Serial.println("Connection failed, trying again in 1s");
-    delay(1000);
-  }
-  Serial.println("connected");
+  TCPConnect();
+  
   client.println("POST /sensors/add HTTP/1.0");
   client.println(contype);
   client.print("Content-Length: ");
@@ -60,7 +59,8 @@ void loop()
   delay(50);
   while (!client.connected()) {
     stop();
-    Serial.println("Connecting...");
+    if(debugInfoTCP)
+      Serial.println("Connecting...");
     if(client.connect(server, 4000))
       break;
   }
@@ -75,14 +75,27 @@ void loop()
   Serial.println(String("val=")+ sensorValue);
 }
 
-void stop(){
-  client.flush();
-  while(client.available()){
-    client.read();
+//Connects to server via TCP
+void TCPConnect(){
+  if(debugInfoTCP)
+    Serial.println("connecting TCP...");
+  while(!client.connect(server,4000)){
+    if(debugInfoTCP)
+      Serial.println("Connection failed, trying again in 1s");
+    delay(1000);
   }
+  if(debugInfoTCP)
+    Serial.println("connected TCP");
+}
+
+void stop(){
+  while(client.available())
+    client.read();
+  client.flush();
   if(!client.connected()){
     client.stop();
-    Serial.println("Stopping...");
+    if(debugInfoTCP)
+      Serial.println("Stopping TCP...");
   }
 }
 
